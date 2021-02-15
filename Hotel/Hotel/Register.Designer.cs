d03c771c2b742bb21d46097570e418aeb14540b4
OB02CGTH <sornsiri1208@gmail.com>
2021-02-15 16:23:59 +0700

    Update Register
    15/2/21 16:23

--- Hotel/Hotel/Register.Designer.cs
+++ Hotel/Hotel/Register.Designer.cs
@@ -30,33 +30,180 @@ namespace Hotel
         private void InitializeComponent()
         {
             this.bt_backlogin = new System.Windows.Forms.Button();
+            this.lb_register = new System.Windows.Forms.Label();
+            this.tb_name = new System.Windows.Forms.TextBox();
+            this.lb_name = new System.Windows.Forms.Label();
+            this.lb_lastname = new System.Windows.Forms.Label();
+            this.tb_lastname = new System.Windows.Forms.TextBox();
+            this.lb_mail = new System.Windows.Forms.Label();
+            this.tb_mail = new System.Windows.Forms.TextBox();
+            this.lb_pass = new System.Windows.Forms.Label();
+            this.tb_pass = new System.Windows.Forms.TextBox();
+            this.lb_passcf = new System.Windows.Forms.Label();
+            this.tb_passcf = new System.Windows.Forms.TextBox();
+            this.bt_regiscf = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // bt_backlogin
             // 
-            this.bt_backlogin.Location = new System.Drawing.Point(12, 12);
+            this.bt_backlogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.bt_backlogin.Location = new System.Drawing.Point(150, 282);
             this.bt_backlogin.Name = "bt_backlogin";
-            this.bt_backlogin.Size = new System.Drawing.Size(75, 23);
+            this.bt_backlogin.Size = new System.Drawing.Size(88, 43);
             this.bt_backlogin.TabIndex = 0;
             this.bt_backlogin.Text = "กลับ";
             this.bt_backlogin.UseVisualStyleBackColor = true;
-            this.bt_backlogin.Click += new System.EventHandler(this.bt_backlogin_Click);
+            // 
+            // lb_register
+            // 
+            this.lb_register.AutoSize = true;
+            this.lb_register.Font = new System.Drawing.Font("FC Home", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.lb_register.Location = new System.Drawing.Point(138, 12);
+            this.lb_register.Name = "lb_register";
+            this.lb_register.Size = new System.Drawing.Size(236, 70);
+            this.lb_register.TabIndex = 1;
+            this.lb_register.Text = "Register";
+            // 
+            // tb_name
+            // 
+            this.tb_name.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.tb_name.Location = new System.Drawing.Point(150, 88);
+            this.tb_name.Name = "tb_name";
+            this.tb_name.Size = new System.Drawing.Size(224, 29);
+            this.tb_name.TabIndex = 2;
+            // 
+            // lb_name
+            // 
+            this.lb_name.AutoSize = true;
+            this.lb_name.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.lb_name.Location = new System.Drawing.Point(80, 91);
+            this.lb_name.Name = "lb_name";
+            this.lb_name.Size = new System.Drawing.Size(64, 24);
+            this.lb_name.TabIndex = 3;
+            this.lb_name.Text = "ชื่อจริง :";
+            // 
+            // lb_lastname
+            // 
+            this.lb_lastname.AutoSize = true;
+            this.lb_lastname.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.lb_lastname.Location = new System.Drawing.Point(64, 126);
+            this.lb_lastname.Name = "lb_lastname";
+            this.lb_lastname.Size = new System.Drawing.Size(80, 24);
+            this.lb_lastname.TabIndex = 5;
+            this.lb_lastname.Text = "นามสกุล :";
+            // 
+            // tb_lastname
+            // 
+            this.tb_lastname.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.tb_lastname.Location = new System.Drawing.Point(150, 123);
+            this.tb_lastname.Name = "tb_lastname";
+            this.tb_lastname.Size = new System.Drawing.Size(224, 29);
+            this.tb_lastname.TabIndex = 4;
+            // 
+            // lb_mail
+            // 
+            this.lb_mail.AutoSize = true;
+            this.lb_mail.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.lb_mail.Location = new System.Drawing.Point(90, 161);
+            this.lb_mail.Name = "lb_mail";
+            this.lb_mail.Size = new System.Drawing.Size(54, 24);
+            this.lb_mail.TabIndex = 7;
+            this.lb_mail.Text = "Mail :";
+            // 
+            // tb_mail
+            // 
+            this.tb_mail.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.tb_mail.Location = new System.Drawing.Point(150, 158);
+            this.tb_mail.Name = "tb_mail";
+            this.tb_mail.Size = new System.Drawing.Size(224, 29);
+            this.tb_mail.TabIndex = 6;
+            // 
+            // lb_pass
+            // 
+            this.lb_pass.AutoSize = true;
+            this.lb_pass.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.lb_pass.Location = new System.Drawing.Point(63, 196);
+            this.lb_pass.Name = "lb_pass";
+            this.lb_pass.Size = new System.Drawing.Size(81, 24);
+            this.lb_pass.TabIndex = 9;
+            this.lb_pass.Text = "รหัสผ่าน :";
+            // 
+            // tb_pass
+            // 
+            this.tb_pass.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.tb_pass.Location = new System.Drawing.Point(150, 193);
+            this.tb_pass.Name = "tb_pass";
+            this.tb_pass.Size = new System.Drawing.Size(224, 29);
+            this.tb_pass.TabIndex = 8;
+            // 
+            // lb_passcf
+            // 
+            this.lb_passcf.AutoSize = true;
+            this.lb_passcf.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.lb_passcf.Location = new System.Drawing.Point(17, 231);
+            this.lb_passcf.Name = "lb_passcf";
+            this.lb_passcf.Size = new System.Drawing.Size(127, 24);
+            this.lb_passcf.TabIndex = 11;
+            this.lb_passcf.Text = "ยืนยันรหัสผ่าน :";
+            // 
+            // tb_passcf
+            // 
+            this.tb_passcf.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.tb_passcf.Location = new System.Drawing.Point(150, 228);
+            this.tb_passcf.Name = "tb_passcf";
+            this.tb_passcf.Size = new System.Drawing.Size(224, 29);
+            this.tb_passcf.TabIndex = 10;
+            // 
+            // bt_regiscf
+            // 
+            this.bt_regiscf.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.bt_regiscf.Location = new System.Drawing.Point(244, 282);
+            this.bt_regiscf.Name = "bt_regiscf";
+            this.bt_regiscf.Size = new System.Drawing.Size(130, 44);
+            this.bt_regiscf.TabIndex = 12;
+            this.bt_regiscf.Text = "สร้างบัญชี";
+            this.bt_regiscf.UseVisualStyleBackColor = true;
+            this.bt_regiscf.Click += new System.EventHandler(this.bt_regiscf_Click);
             // 
             // Register
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(422, 366);
+            this.Controls.Add(this.bt_regiscf);
+            this.Controls.Add(this.lb_passcf);
+            this.Controls.Add(this.tb_passcf);
+            this.Controls.Add(this.lb_pass);
+            this.Controls.Add(this.tb_pass);
+            this.Controls.Add(this.lb_mail);
+            this.Controls.Add(this.tb_mail);
+            this.Controls.Add(this.lb_lastname);
+            this.Controls.Add(this.tb_lastname);
+            this.Controls.Add(this.lb_name);
+            this.Controls.Add(this.tb_name);
+            this.Controls.Add(this.lb_register);
             this.Controls.Add(this.bt_backlogin);
             this.Name = "Register";
             this.Text = "Register";
-            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Register_FormClosed);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button bt_backlogin;
+        private System.Windows.Forms.Label lb_register;
+        private System.Windows.Forms.TextBox tb_name;
+        private System.Windows.Forms.Label lb_name;
+        private System.Windows.Forms.Label lb_lastname;
+        private System.Windows.Forms.TextBox tb_lastname;
+        private System.Windows.Forms.Label lb_mail;
+        private System.Windows.Forms.TextBox tb_mail;
+        private System.Windows.Forms.Label lb_pass;
+        private System.Windows.Forms.TextBox tb_pass;
+        private System.Windows.Forms.Label lb_passcf;
+        private System.Windows.Forms.TextBox tb_passcf;
+        private System.Windows.Forms.Button bt_regiscf;
     }
 }
--- Hotel/Hotel/Register.cs
+++ Hotel/Hotel/Register.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
 namespace Hotel
@@ -28,6 +29,12 @@ namespace Hotel
         private void Register_FormClosed(object sender, FormClosedEventArgs e)
         {
             fflogin.Show();
+        }
+
+        private void bt_regiscf_Click(object sender, EventArgs e)
+        {
+            fflogin.Show();
+            this.Close();
         }
     }
 }
